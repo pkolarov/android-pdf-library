0adc75c51e8bdd77f8181bbd88d9c4b8d37dfae9
pkolarov <pkolarov@gmail.com>
2015-04-03 21:54:12 +0200

    clean up

--- src/main/obj/local/armeabi-v7a/objs/mupdfcore/source/fitz/time.o.d
+++
@@ -1,2 +0,0 @@
-H:/DATA-SB/android_work/android-pdf-library/src/main//obj/local/armeabi-v7a/objs/mupdfcore/source/fitz/time.o: \
- H:/DATA-SB/android_work/android-pdf-library/src/main//jni/source/fitz/time.c
